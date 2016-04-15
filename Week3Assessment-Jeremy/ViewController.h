4e99ee8eac283429fcfec97f3dbf64bcbddc8b20
Jeremy Ong <jeremy.ongws@gmail.com>
2016-04-15 09:35:44 +0800

    Initial Commit

---
+++ Week3Assessment-Jeremy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Week3Assessment-Jeremy
+//
+//  Created by Jeremy Ong on 15/04/2016.
+//  Copyright © 2016 Jeremy Ong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
